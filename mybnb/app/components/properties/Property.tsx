670119149993cfa90d1988169bb9e384e6177fed
Sumit Choudhary <sumit.rc619@gmail.com>
2024-10-10 23:03:13 +0530

    Property

--- mybnb/app/components/properties/Property.tsx
+++ mybnb/app/components/properties/Property.tsx
@@ -1,14 +1,42 @@
-import React from "react"
-import PropertyListItem from "./PropertyListItem"
+"use client";
 
-const Property = () => {
-  return (
-    <>
-        <PropertyListItem/>
-        <PropertyListItem/>
-        <PropertyListItem/>
-    </>
-  )
+import React, { useEffect, useState } from "react";
+import PropertyListItem from "./PropertyListItem";
+
+export type PropertyType = {
+  id: string;
 }
 
-export default Property
+const Property = () => {
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+
+  const getResponse = async () => {
+    const url = "http://localhost:8000/api/properties";
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("json", json);
+        setProperties(json.data);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  useEffect(() => {
+    getResponse();
+  }, []);
+
+  return (
+    <>
+      {properties.map((property) => {
+        return (<PropertyListItem
+          key={property.id}
+          // property={property}
+          />);
+      })}
+    </>
+  );
+};
+
+export default Property;
